52f22a147e4538d40ec475a8eb3c7e6784371d50
HA2ZB <140746138+HA2ZB@users.noreply.github.com>
2023-09-03 17:34:22 +0200

    Update HDLC_MX614.cpp

--- HDLC_MX614.cpp
+++ HDLC_MX614.cpp
@@ -75,190 +75,223 @@ Library internal functions
 // timer function, returns a boolean value if the time passed from the Set_Time is shorter than the argument
 boolean Timer_Running(long TimerValue) {
 
-  return ((millis() - Set_Time) < TimerValue);
-}
+  return ((millis() - Set_Time) < TimerValue);    // compares the current time (milliseconds since starting the program) to the set time (global variable, set at the time we want to start the counter) and returns the boolean result
+}                                                 // if the difference is smaller waht is provided in the argument (TimerValue) the return value is positive, meaning the timer is still running
 
 
-// FCS calculation step (1 bit)
+// single bit FCS calculation step, this function will be called regularly by the FCS word calculation routine
+// argument: the data bit we want to process (lowest bit of MSGbit)
+// for detailed explanation see CRC-CCITT and ISO 3309 specifications
+
 void FCScalc(byte MSGbit) {
 
-  boolean XOR;
+  boolean XOR;    // internal variable for boolean operation
 
-  XOR = (lowByte(FCS) & 0x01) ^ MSGbit == 0x01;
+  XOR = (lowByte(FCS) & 0x01) ^ MSGbit == 0x01;   // XOR operation on the lowest bit of FCS word (global variable) and the message bit we are processing
 
-  FCS = FCS >> 1;
+  FCS = FCS >> 1;   // shift right FCS word by one
 
   if (XOR) {
 
-    FCS = FCS ^ 0x8408;
+    FCS = FCS ^ 0x8408;   // if the result is 1 (true) we apply the generator polynom as a XOR operation
   }
 }
 
 
 // FCS calculator fuction, calculates the FCS word for the whole packet (argument is the number of frame bytes)
+// for detailed explanation see CRC-CCITT and ISO 3309 specifications
+
 word CalculatedFCS(int FrameBytes) {
 
-  byte PacketByte;
+  byte PacketByte;  // internal variable to process a specific byte in the frame
 
-  FCS = 0xFFFF;
+  FCS = 0xFFFF;     // initial value of FCS according to the specification
 
-  for (int i = 0; i <= FrameBytes; i++) {
+  for (int i = 0; i <= FrameBytes; i++) {     // we do it for all frame bytes (except flags and incoming FCS)
 
-    PacketByte = HDLC_Packet[i];
+    PacketByte = HDLC_Packet[i];              // we take the first / next message byte
 
-    for (int j = 0; j <= 7; j++) {
+    for (int j = 0; j <= 7; j++) {            // we do the FCS process for each bits
 
-      FCScalc(PacketByte & 0x01);
+      FCScalc(PacketByte & 0x01);             // calling the sub-function above
 
-      PacketByte = PacketByte >> 1;
+      PacketByte = PacketByte >> 1;           // shift the byte right by one to get the next bit
     }
   }
 
-  FCS = FCS ^ 0xFFFF;
+  FCS = FCS ^ 0xFFFF;                         // XOR mask on the calculated word according to the specification
 
-  return FCS;
+  return FCS;                                 // return the calculated FCS word
 }
 
+// this function gets the incoming FCS word from the received data frame
 word FrameFCS() {
 
-  word Frame_FCS_value;
+  return (HDLC_Packet[FrameLength] * 256 + HDLC_Packet[FrameLength - 1]);   // we simply create a word value from the 2 FCS bytes at the end of the incoming frame, and return as function value
 
-  Frame_FCS_value = HDLC_Packet[FrameLength] * 256 + HDLC_Packet[FrameLength - 1];
-
-  return Frame_FCS_value;
 }
 
-
+// this sub-fuction is doing the sampling on the modem's RXD output digital signal
+// and returns the received bit as 1 (true) or 0 (false)
 
 boolean ReceiveBit() {
 
-  boolean bitIn = true;  // internal variable for returning the value
+  boolean bitIn = true;  // internal variable for returning the value. we set this to 'true' for the initial sampling
 
-  while (bitIn && (micros() < TimeOut)) {
+  while (bitIn && (micros() < TimeOut)) {   // we assume the next incoming bit is 1, and we wait for 1 bit time before sampling
 
-    signalT = (digitalRead(RX) == HIGH);  // we read the RX signal (modem RXD)
-    bitIn = signalT == signalT_1;
-    signalT_1 = signalT;
+    signalT = (digitalRead(RX) == HIGH);  // sampling - we read the RX signal (modem RXD)
+    bitIn = signalT == signalT_1;         // if there is no change related to the previous (T-1) sample, the received bit is 1 (no return to zero coding)
+    signalT_1 = signalT;                  // we store this sample as T-1 sample for the next sampling
   }
 
-  if (bitIn) {
-    TimeOut = micros() + 830;
+  if (bitIn) {                            // if we received a 1 it means, there were no switch on the logic values, we did not get an edge to synchronize our sampler timing
+    TimeOut = micros() + 830;             // so we use a quasi standard 830 microsec delay for the next sample. this should not be much longer, not to lose the sync if more 1s are coming
   } else {
-    TimeOut = micros() + 1000;
-  }  // 820-840 960-1200
-
-  return bitIn;  // we return the bit value
+    TimeOut = micros() + 1000;            // if we received a 0, we had an edge, so we now can lock our timing to that. we apply here a bit longer delay to the next sample
+  }                                       // so we position the time somewhere in the midle of the next byte, not ot miss a possible next edge
+                                          // these timing ranges work in this solution: 820-840 960-1200
+  return bitIn;                           // we return the bit value
 }
+
+
+// sub fuction to get the incoming REAL databit
+// manages AX.25 bit-stuffing
 
 boolean GetRXD() {
 
-  boolean RXbit;
+  boolean RXbit;    // internal variable for return value
+  boolean Stuff_Zero_Expected;
 
-  RXbit = ReceiveBit();
+  Stuff_Zero_Expected = (Stuff == 5);
 
-  if (RXbit) {
+  RXbit = ReceiveBit();   // we call the function above to get the PHYSICAL incoming bit
 
-    Stuff++;  // we increment the 1's counter
+  if (RXbit) {            // if this is 1
 
-  } else {
+    Stuff++;              // we increment the 1's counter
 
-    if (Stuff == 5) {
+  } else {                // if 0 received
 
-      RXbit = ReceiveBit();
+    Stuff = 0;            // we reset the stuff counter
+
+    if (Stuff_Zero_Expected) {     // we check if there were 5 consecutive 1s before
+
+      RXbit = ReceiveBit();   // if yes, this is a stuffing 0, so we drop it and do the next sampling, that will be a REAL HDLC bit
+
+      if (RXbit) {            // if the next bit is 1
+
+        Stuff = 1;            // we start counting the 1 bits again
+
+      }
+
     }
-
-    Stuff = 0;
+   
   }
 
-  return RXbit;
+  return RXbit;               // we return the received (real) HDLC bit
+
 }
 
-byte ReceiveByte(boolean LookForFlag) {
+// byte level HDLC receive function
 
-  byte RXbyte = 0x00;  // set the initial value of the byte to return
+byte ReceiveByte(boolean LookForFlag) {     // argument: if true, we are looking for a HDLC flag
 
-  int i;  // define for cycle variable
+  byte RXbyte = 0x00;                       // set the initial value of the byte to return
 
-  if (LookForFlag) {  // if we are looking for a flag, it can come at any time
+  if (LookForFlag) {                        // if we are looking for a flag, it can come at any time
 
-    while ((RXbyte != 0x7E) && Timer_Running(RX_TimeOut)) {  // so we keep scanning until we have an incoming flag (0x7E)
+    while ((RXbyte != 0x7E) && Timer_Running(RX_TimeOut)) {       // so we keep scanning until we have an incoming flag (0x7E), or the timer counts down
 
-      RXbyte = RXbyte >> 1;  // we shift the byte to the right by 1
+      RXbyte = RXbyte >> 1;                 // we shift the byte to the right by 1
 
-      if (GetRXD()) { RXbyte = RXbyte | 0x80; }  // we call the bit reading function and we tell to it that we are looking for a flag, no 0 dropping needed
+      if (GetRXD()) { RXbyte = RXbyte | 0x80; }  // we call the bit reading function
                                                  // if it returns 'true' (1), we set the highest bit to 1 otherwise it remains 0
     }                                            // and keep this doing until we have a flag byte
 
   }
 
-  else {  // if we are not looking for a flag, we read exactly the next 8 bits (w/o zeros to be dropped)
+  else {                                        // if we are not looking for a flag, we read exactly the next 8 bits (w/o zeros to be dropped)
 
-    for (i = 0; i < 8; i++) {  // we simply read the next 8 bits (the called function will drop zeros if needed) and put it into the byte to be returned
+    for (int i = 0; i < 8; i++) {               // we simply read the next 8 bits (the called function will drop zeros if needed) and put it into the byte to be returned
 
-      RXbyte = RXbyte >> 1;
+      RXbyte = RXbyte >> 1;                     // shift right the databyte
 
-      if (GetRXD()) { RXbyte = RXbyte | 0x80; }  // false argument tells it should drop zero if needed
+      if (GetRXD()) { RXbyte = RXbyte | 0x80; }  // read the next databit and set the highest bit of the RXbyte accordingly
     }
   }
 
   return RXbyte;  // we return the received byte
+
 }
 
+// sub-function for transmitting data, this changes the modem TXD input level if zero to be transmitted (NRZ coding)
 
 void flipOut() {
 
-  Stuff = 0;
+  Stuff = 0;                              // we reset the stuff counter, as we have a 0 to transmit
 
-  bitOut = !bitOut;
+  bitOut = !bitOut;                       // negate the bitOut global boolean variable
 
-  if (bitOut) digitalWrite(TX, HIGH);
-  else digitalWrite(TX, LOW);
+  if (bitOut) digitalWrite(TX, HIGH);     // if it is 'true' (1), we set the TXD modem input to HIGH
+  else digitalWrite(TX, LOW);             // if 'false' (0), we set the TXD to LOW
 }
 
-void transmit() {
 
-  while (digitalRead(RDY) == HIGH) {}
+// bit transmission sub-function with modem sync control
+// the MX/FX614 RDY-CLK handshake ensures the accurate 1200 bps timing
 
-  delayMicroseconds(30);
+void transmit() {                   
 
-  digitalWrite(CLK, HIGH);
+  while (digitalRead(RDY) == HIGH) {}     // we wait for the modem to drop the RDY output to 0, indicating that it is ready to receive the next bit
 
-  delayMicroseconds(30);
+  delayMicroseconds(30);                  // if dropped, we wait 30 microsecs (see modem datasheet)
 
-  digitalWrite(CLK, LOW);
+  digitalWrite(CLK, HIGH);                // and we set CLK modem input to HIGH, latching the TXD input to the modem's shift register
+
+  delayMicroseconds(30);                  // we wait 30 microsecs (see modem datasheet)
+
+  digitalWrite(CLK, LOW);                 // and reset CLK
 }
+
+
+// global variable and setter needed to manage the TX counter (counting HDLC frame bytes)
 
 void Reset_TX_Counter() {
 
   TX_Counter = 0;
 }
 
-void Send_HDLC_Byte(byte TXByte, boolean FlagByte) {
+// sub-fuction to manage HDLC byte transmission and related bit stuffing
 
-  int k;
-  byte sendBit;
+void Send_HDLC_Byte(byte TXByte, boolean FlagByte) {      // arguments: byte to send and boolean to indicate if this is a FlagByte (no bit stuffing needed)
 
-  for (k = 0; k < 8; k++) {
+  byte sendBit;                                           // internal variable for bit level process
 
-    sendBit = TXByte & 0x01;
+  for (int k = 0; k < 8; k++) {                           // for all the 8 bits...
 
-    if (sendBit == 0) flipOut();
-    else {
+    sendBit = TXByte & 0x01;                              // we take the last bit of the byte to transmit
 
-      Stuff++;
+    if (sendBit == 0) flipOut();                          // if this is 0, we switch the TXD level (NRZ coding)
+    
+    else {                                                // if 1
 
-      if (!FlagByte && (Stuff == 5)) {
+      Stuff++;                                            // we increment the stuff counter
 
-        transmit();
+      if (!FlagByte && (Stuff == 5)) {                    // if this is not a flagbyte and we already have 5 consecutive 1s
 
-        flipOut();
+        transmit();                                       // we transmit this 5th 1-value bit
+
+        flipOut();                                        // and we insert a stuff zero by switching the TXD level
       }
     }
 
-    TXByte = TXByte >> 1;
+    TXByte = TXByte >> 1;                                 // we shift right the TX byte, next bit will come
 
-    transmit();
+    transmit();                                           // we transmit the current bit
+
   }
+
 }
 
 
@@ -268,10 +301,12 @@ Public functions
 -------------------------------------------------------------------------------------------------------------------------------------------
 */
 
+// modem initialization function
 
 void Begin_Modem() {
 
-  pinMode(TX, OUTPUT);
+  // settings Arduino digital ports to input or output respectively
+  pinMode(TX, OUTPUT);    
   pinMode(PTT, OUTPUT);
   pinMode(CLK, OUTPUT);
   pinMode(RDY, INPUT);
@@ -280,32 +315,42 @@ void Begin_Modem() {
   pinMode(M0, OUTPUT);
   pinMode(M1, OUTPUT);
 
+  // setting initial output levels
   digitalWrite(M0, HIGH);
   digitalWrite(M1, HIGH);
   digitalWrite(TX, HIGH);
   digitalWrite(CLK, HIGH);
   digitalWrite(PTT, LOW);
 
-  // delay(1000);
 }
+
+// RX timeout setter
+// the time in millisecs we are waiting for a valid frame (flagbytes) after data carrier detection
 
 void Set_RXTimeOut(long RXTimeOutValue) {
 
   RX_TimeOut = RXTimeOutValue;
 }
 
+// TX timeout setter
+// the time in millisecs we are waiting in case busy channel (carrier detected) before sending the HDLC frame 
+
 void Set_TXTimeOut(long TXTimeOutValue) {
 
   TX_TimeOut = TXTimeOutValue;
 }
+
+// function to set the modem to idle state
 
 void Modem_Off() {
 
   digitalWrite(M0, HIGH);
   digitalWrite(M1, HIGH);
 
-  delay(200);
+  delay(200);   // we may not need this delay, but protects switching the modem on-off too fast
 }
+
+// function to set the modem to 1200 bps RX mode
 
 void Modem_RX() {
 
@@ -316,9 +361,11 @@ void Modem_RX() {
   delay(25);
 }
 
+// function to set the modem to 1200 bps TX mode
+
 void Modem_TX() {
 
-  bitOut = false;
+  bitOut = false;     // we set the TXD to low, so we set this global variable accordingly to 'false'
 
   digitalWrite(CLK, LOW);
   digitalWrite(TX, LOW);
@@ -329,36 +376,48 @@ void Modem_TX() {
   delay(300);
 }
 
+// function to key PTT output
+
 void PTT_On() {
 
   digitalWrite(PTT, HIGH);
 
-  delay(PTT_Delay);
+  delay(PTT_Delay);  // PTT delay can be set by function, this is the time in millisecs between keying PTT and starting the transmission
 }
+
+// function to unkey PTT output
 
 void PTT_Off() {
 
   digitalWrite(PTT, LOW);
 }
 
+// function to set PTT delay
+
 void Set_PTT_Delay(int PTT_Delay_Value) {
 
-  PTT_Delay = PTT_Delay_Value;
+  PTT_Delay = PTT_Delay_Value;    // this is the time in millisecs between keying PTT and starting the transmission
 }
+
+// function to return the status of modem DET output
+// if 'true', the modem detects a carrier on the channel
 
 boolean Carrier_Detected() {
 
   boolean CD = false;
 
-  if (digitalRead(DET) == HIGH) {
+  if (digitalRead(DET) == HIGH) {     // first sample
 
-    delay(15);
+    delay(15);                        // 15 ms delay
 
-    CD = (digitalRead(DET) == HIGH);
+    CD = (digitalRead(DET) == HIGH);  // second sample. if both samples are HIGH, we have a real carrir, not just a pin impulse, which can come from the Bluetooth
+  
   }
 
-  return CD;
+  return CD;      // we return the function value
+
 }
+
 
 int HDLC_Frame_Available() {
 
@@ -372,7 +431,7 @@ int HDLC_Frame_Available() {
 
   if (Carrier_Detected()) {  //signal detected, we should have flag(s) incoming
 
-    Set = millis();
+    Set_Time = millis();
 
     ByteIn = ReceiveByte(true);
 
